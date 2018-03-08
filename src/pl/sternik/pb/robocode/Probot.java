61bac4bf748bad6e4053a32f7ed735a32b192279
Pawel Babiuch <pawel.babiuch@edu.uekat.pl>
2018-03-08 14:03:01 +0100

    no message

--- src/pl/sternik/pb/robocode/Probot.java
+++ src/pl/sternik/pb/robocode/Probot.java
@@ -1,11 +1,36 @@
 package pl.sternik.pb.robocode;
 
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
+import java.awt.Color;
 
-public class Probot extends Robot {
+import robocode.*;
+import java.awt.Color;
 
-	public void run() {
-		
+public class Probot extends AdvancedRobot {
+	
+
+}
+
+class Enemy {
+	/*
+	 * ok, we should really be using accessors and mutators here,
+	 * (i.e getName() and setName()) but life's too short.
+	 */
+	String name;
+	public double bearing;
+	public double head;
+	public long ctime; //game time that the scan was produced
+	public double speed;
+	public double x,y;
+	public double distance;
+	public double guessX(long when)
+	{
+		long diff = when - ctime;
+		return x+Math.sin(head)*speed*diff;
 	}
+	public double guessY(long when)
+	{
+		long diff = when - ctime;
+		return y+Math.cos(head)*speed*diff;
+	}
+
 }
